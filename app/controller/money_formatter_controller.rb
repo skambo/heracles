7b1ab527504298d0e4416145ff63c9909f76fef2
Sandra Kambo <swairimu2002@gamil.com>
2019-10-05 18:14:54 +0300

    Added validation to the controller and a dependency to display flash messages

--- app/controller/money_formatter_controller.rb
+++ app/controller/money_formatter_controller.rb
@@ -1,9 +1,12 @@
 require_relative '../../app/helper'
+require 'rack-flash'
 
 class MoneyFormatterController < Sinatra::Base
+    use Rack::Flash
 
     configure do                            #configure method tells Sinatra where to find views
       set :views, 'app/views'
+      enable :sessions
     end
 
     get "/" do                              #get method displays the form
@@ -12,7 +15,16 @@ class MoneyFormatterController < Sinatra::Base
 
     post "/format" do                       #post method receives input from the user, and does the validation and returns the formatted amount back to the user
         amount = params[:amount]
-        @formatted_amount = Helper.format_money(amount)
-        erb :formatted
+        if amount.empty? || ! is_number?(amount)
+            flash[:message] = "Please enter valid input"
+            erb :formatted
+        else
+            @formatted_amount = Helper.format_money(amount.to_f)
+            erb :formatted
+        end
+    end
+    private
+    def is_number?(string)
+        true if Float(string) rescue false
     end
 end
