1c9e7217082de9fbd7c8befd5bfe0995a8ddf03c
Sandra Kambo <swairimu2002@gamil.com>
2019-10-06 22:04:18 +0300

    Finalize UI test

--- tests/ui/form_test.rb
+++ tests/ui/form_test.rb
@@ -1,22 +1,18 @@
-# frozen_string_literal: true
-
 require 'selenium-webdriver'
-require_relative '../../tests/test_helper'
-require 'webdrivers/chromedriver'
-
+require 'rspec'
+require 'chromedriver-helper'
 
 
 describe 'testing the form end to end' do
-  if 'submits an amount'
-    driver = Selenium::WebDriver::Chrome::Service.for :chrome
-    driver.navigate.to 'http://127.0.0.1:9393/api/v1/formatAmount'
+  it 'submits an amount' do
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to 'http://127.0.0.1:9393'
 
     driver.find_element(id: 'money').send_keys(2000)
     driver.find_element(id: 'submit').click
-    wait = Selenium::WebDriver::wait.new(timeout:10)
+    #wait = Selenium::WebDriver::wait.new(timeout:10)
     #wait.until {driver.find_element(i)}
-    expect(driver.find_element(id: 'format').text).to eql ("Formatted Amount: 2 000.00")
+    expect(driver.find_element(id: 'format').text).to eql("Formatted Amount: 2 000.00")
     driver.quit
-
-end
+   end
 end
