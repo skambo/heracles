19346d854ee79836d779f73838783e330393d0f6
Sandra Kambo <swairimu2002@gamil.com>
2019-10-07 11:00:11 +0300

    Add a UI test if no input is entered

--- tests/ui/form_test.rb
+++ tests/ui/form_test.rb
@@ -12,5 +12,16 @@ describe 'testing the form end to end' do
     driver.find_element(id: 'submit').click
     expect(driver.find_element(id: 'format').text).to eql("Formatted Amount: 2 000.00")
     driver.quit
-   end
+  end
+end
+
+describe 'test the form with no amount input' do
+  it 'no input amount given' do
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to 'http://127.0.0.1:9393'
+
+    driver.find_element(id: 'submit').click
+    expect(driver.find_element(id: 'error_banner').text).to eql("Please enter valid input")
+    driver.quit
+  end
 end
